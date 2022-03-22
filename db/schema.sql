132b1e6cc97897c6dee943c3f30d6d2f3899a0d9
jaredpennington <94023319+jaredpennington@users.noreply.github.com>
2022-03-22 19:04:32 -0400

    office hours push

--- db/schema.sql
+++ db/schema.sql
@@ -1,27 +1,32 @@
-DROP DATABASE IF EXISTS employeeTracker_db;
-CREATE DATABASE employeeTracker_db;
+DROP DATABASE IF EXISTS employeetracker;
 
-USE employeeTracker_db;
+CREATE DATABASE employeetracker;
 
-CREATE TABLE department (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+USE employeetracker;
+
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS employees;
+
+CREATE TABLE departments (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) UNIQUE NOT NULL
 );
 
-CREATE TABLE role (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE roles (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
-    salary DECIMAL UNSIGNED NOT NULL,
-    department_id INT UNSIGNED NOT NULL,
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id)
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
-CREATE TABLE employee (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE employees (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    role_id INT UNSIGNED NOT NULL,
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id),
-    manager_id INT UNSIGNED,
-    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id)
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id),
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employees(id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,16 +1,27 @@
-USE employeeTracker_db;
 
-INSERT INTO department (name) VALUES
+INSERT INTO departments (name) VALUES
     ('Sales'),
     ('Engineering'),
     ('Marketing'),
     ('Cheif Offiers');
 
-INSERT INTO role (title, salary, department_id) VALUES
-    ('Sales Manager', 80000, 1),
-    ('Sales Assistant', 60000, 1);
+INSERT INTO roles (title, salary, department_id) VALUES
+    ('Sales Manager', 110000, 1),
+    ('Sales Assistant', 95000, 1),
+    ('Senior Engineer', 120000, 2),
+    ('Junior Enginneer', 95000, 2),
+    ('Marketing Director', 110000, 3),
+    ('Marketing Producer', 95000, 3),
+    ('CEO', 250000, 4),
+    ('CFO', 240000, 4),
+    ('President', 180000, 4),
+    ('Vice President', 160000, 4);
+
+
     -- Next positions can go directly underneath just replace semicolon witha  comma
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
-    ('Jared', 'Pennington', 1, NULL),
-    ('Aaron', 'Cent', 2, 1);
+INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES
+    ('Jared', 'Pennington', 1, 2),
+    ('Aaron', 'Cent', 1, 3),
+    ('Sam', 'Samuels', 8, 4),
+    ('Carl', 'Carlson', 7, 4);
